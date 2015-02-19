0361d877244bbccbf51fc62d50cdf75fc9ce29b1
mikiobraun <mikiobraun@gmail.com>
2015-02-19 22:28:33 +0100

    example for side data

---
+++ bdc-project/src/main/java/bdc/advanced/SideData.java
@@ -0,0 +1,81 @@
+package bdc.advanced;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.net.URI;
+
+/**
+ * Example to show the use of side data.
+ */
+public class SideData extends Configured implements Tool {
+
+  public static void main(String[] args) throws Exception {
+    int exitCode = ToolRunner.run(new SideData(), args);
+    System.exit(exitCode);
+  }
+
+  static class SideDataMapper extends Mapper<Text, IntWritable, Text, IntWritable> {
+    @Override
+    protected void setup(Context context) {
+      try {
+        URI[] paths = context.getCacheFiles();
+
+        if (paths != null) {
+          for (URI p : paths) {
+            System.out.println("Found Path " + p);
+          }
+        } else {
+          System.out.println("paths = null!");
+        }
+
+        File f = new File("SIDE-DATA");
+        System.out.println("Can I read this? " + f.canRead());
+        BufferedReader r = new BufferedReader(new FileReader(f));
+        System.out.println("Contents = \"" + r.readLine() + "\"");
+        r.close();
+      } catch (Exception ex) {
+        throw new RuntimeException(ex);
+      }
+    }
+  }
+
+  static class SideDataReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+  }
+
+  public int run(String[] strings) throws Exception {
+    Configuration conf = getConf();
+    Job job = Job.getInstance(conf, "SideDate");
+
+    job.setJarByClass(SideData.class);
+
+    job.setMapperClass(SideDataMapper.class);
+    job.setReducerClass(SideDataReducer.class);
+    job.setNumReduceTasks(1);
+
+    FileInputFormat.addInputPath(job, new Path("README"));
+    Path outputDir = new Path("out");
+    FileOutputFormat.setOutputPath(job, outputDir);
+
+    // Delete output if exists
+    FileSystem hdfs = FileSystem.get(conf);
+    if (hdfs.exists(outputDir) && hdfs.isDirectory(outputDir))
+      hdfs.delete(outputDir, true);
+
+    return job.waitForCompletion(true) ? 0 : 1;
+  }
+}
