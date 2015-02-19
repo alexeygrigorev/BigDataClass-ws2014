c6b5ddb2fb8df609b3f3daafb63838ed41e0518e
Mikio L. Braun <mikiobraun@gmail.com>
2015-02-19 14:48:48 +0100

    one file at least

---
+++ setup.sh
@@ -0,0 +1,6 @@
+export BDC=/home/BIGdata/bdc-ws14
+export M2_HOME=$BDC/opt/apache-maven-3.2.5
+export IDEA_HOME=$BDC/opt/idea-IC-139.1117.1
+export PATH=$M2_HOME/bin:$IDEA_HOME/bin:$PATH
+
+export HADOOP_CONF_DIR=
