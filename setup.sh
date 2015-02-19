28900a4ee78ad4eac8d8b8f7ed737efbcaa1dd60
Mikio L. Braun <mikiobraun@gmail.com>
2015-02-19 14:51:36 +0100

    g

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ export M2_HOME=$BDC/opt/apache-maven-3.2.5
 export IDEA_HOME=$BDC/opt/idea-IC-139.1117.1
 export PATH=$M2_HOME/bin:$IDEA_HOME/bin:$PATH
 
-export HADOOP_CONF_DIR=
+export HADOOP_CONF_DIR=$BDC/conf/local
