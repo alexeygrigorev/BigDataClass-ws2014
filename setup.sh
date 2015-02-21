bd4bea82c9b53a778fbf85b1aaed97fab1098457
mikiobraun <mikiobraun@gmail.com>
2015-02-21 22:25:53 +0100

    added spark path

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 export BDC=/home/BIGdata/bdc-ws14
 export M2_HOME=$BDC/opt/apache-maven-3.2.5
 export IDEA_HOME=$BDC/opt/idea-IC-139.1117.1
-export PATH=$M2_HOME/bin:$IDEA_HOME/bin:$PATH
+export SPARK_HOME=$BDC/opt/spark-1.2.1-bin-hadoop2.4
+export PATH=$M2_HOME/bin:$IDEA_HOME/bin:$SPARK_HOME/bin:$PATH
 
 export HADOOP_CONF_DIR=$BDC/conf/local
