5e3f494ccdf41887a3f8ee07c8177d310df98a8b
mikiobraun <mikiobraun@gmail.com>
2015-02-23 09:36:42 +0100

    Renamed class for exercise 1

--- bdc-project/src/main/java/bdc/ex1/MinMaxMeanValue.java
+++ bdc-project/src/main/java/bdc/ex1/MinMaxMeanValue.java
@@ -4,5 +4,5 @@ package bdc.ex1;
  * Write a map reduce job which computes the maximal temperature if the input is given
  * as year, temp pairs.
  */
-public class MaxTemperature {
+public class MinMaxMeanValue {
 }
